5ecaa98614386babce7944eed57648504238185d
majuanjuan <2891529590@qq.com>
2016-05-31 11:48:23 +0800

    firstday11:48

---
+++ MePlusiPhone/VideoViewController.h
@@ -0,0 +1,13 @@
+//
+//  VideoViewController.h
+//  MePlusiPhone
+//
+//  Created by sks on 16/5/31.
+//  Copyright © 2016年 MePlus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoViewController : UIViewController
+
+@end
